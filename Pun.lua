83ff09d33e1024dd91b16eae175aefae33dfbfc8
krnlexecutor735-cell <krnlexecutor735@gmail.com>
2026-04-15 12:41:16 +0700

    Change window icon from 'player' to 'github'

--- Pun.lua
+++ Pun.lua
@@ -39,7 +39,7 @@ local Window = WindUI:CreateWindow({
 
 Window:Tag({
     Title = "v1.1.2",
-    Icon = "player",
+    Icon = "github",
     Color = Color3.fromHex("#30ff6a"),
     Radius = 0, -- from 0 to 13
 })
